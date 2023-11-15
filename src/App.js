bb0f7f0448576e814b551ec65c5eecd04631ba0a
Wilson Siaw <wilsonsiawse@gmail.com>
2023-11-15 10:39:05 +0200

    Build a card UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import sedan from '../src/images/icon-sedans.svg';
+import suv from '../src/images/icon-suvs.svg'
+import luxury from '../src/images/icon-luxury.svg';
+import Card from './components/Card';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card-container'>
+      <div className='sedan'>
+        <img src={sedan} alt='logo of a sedan'/>
+        <Card
+        heading='SEDANS'
+        body='Choose a sedan for its affordability and excellent
+        fuel economy. I deal for cruising in the city or on your next
+        road trip'
+        button='Learn More'
+        />
+      </div>
+      <div className='suv'>
+        <img src={suv} alt='logo of an suv'/>
+        <Card
+        heading='SUV'
+        body='Take an SUV for its spacious interior, power, and versatility.
+        Perfect for your next family vacation and off-road adventures.'
+        button='Learn More'
+        />
+      </div>
+      <div className='luxury'>
+        <img src={luxury} alt='logo of a luxury car'/>
+        <Card
+        heading='LUXURY'
+        body='Cruise in the best car brands without the bloated prices.
+        Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+        button='Learn More'
+        />
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Card.css'
 
 function Card({ heading, body, button }) {
   return (
-    <div>
-      <h1>{heading}</h1>
-      <p>{body}</p>
-      <button>{button}</button>
+    <div className='card'>
+      <h1 className='heading'>{heading}</h1>
+      <p className='body'>{body}</p>
+      <button className='btn'>{button}</button>
     </div>
   )
 }
